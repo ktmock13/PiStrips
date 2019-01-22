d415b973d5d9d94d6c03bfeb7345e1f024f93012
ktmock13 <ktmock13@gmail.com>
2019-01-21 17:13:10 -0800

    update

--- accessories/Strip_accessory.js
+++ accessories/Strip_accessory.js
@@ -79,6 +79,8 @@ var LightController = {
   setBrightness: function(brightness) { //set brightness
     if(this.outputLogs) console.log("Setting '%s' brightness to %s", this.name, brightness);
     this.brightness = brightness;
+    ws281x.setBrightness((brightness/100) * 255);
+
   },
 
   getBrightness: function() { //get brightness
