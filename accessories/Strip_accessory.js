def4057ad51e30b0bbbf2de4eb680a8d96f620ec
ktmock13 <ktmock13@gmail.com>
2019-01-21 17:41:10 -0800

    update

--- accessories/Strip_accessory.js
+++ accessories/Strip_accessory.js
@@ -18,41 +18,43 @@ function _runStaticColor(h, s, l) {
 }
 
 function _runRainbow() {
-  console.log('..._runRainbow called...');
-  var NUM_LEDS = 80,
-  pixelData = new Uint32Array(NUM_LEDS);
+  if(!interval) { //check if it's already running
+    console.log('..._runRainbow called...');
+    var NUM_LEDS = 80,
+    pixelData = new Uint32Array(NUM_LEDS);
 
-  ws281x.init(NUM_LEDS);
+    ws281x.init(NUM_LEDS);
 
-  // ---- trap the SIGINT and reset before exit
-  process.on('SIGINT', function () {
-    ws281x.reset();
-    process.nextTick(function () { process.exit(0); });
-  });
+    // ---- trap the SIGINT and reset before exit
+    process.on('SIGINT', function () {
+      ws281x.reset();
+      process.nextTick(function () { process.exit(0); });
+    });
 
 
-  // ---- animation-loop
-  var offset = 0;
-  interval = setInterval(function () {
-    for (var i = 0; i < NUM_LEDS; i++) {
-      pixelData[i] = colorwheel((offset + i) % 256);
+    // ---- animation-loop
+    var offset = 0;
+    interval = setInterval(function () {
+      for (var i = 0; i < NUM_LEDS; i++) {
+        pixelData[i] = colorwheel((offset + i) % 256);
+      }
+      offset = (offset + 1) % 256;
+      ws281x.render(pixelData);
+    }, 1000 / 30);
+
+    console.log('Press <ctrl>+C to exit.');
+
+    // rainbow-colors, taken from http://goo.gl/Cs3H0v
+    function colorwheel(pos) {
+      pos = 255 - pos;
+      if (pos < 85) { return rgb2Int(255 - pos * 3, 0, pos * 3); }
+      else if (pos < 170) { pos -= 85; return rgb2Int(0, pos * 3, 255 - pos * 3); }
+      else { pos -= 170; return rgb2Int(pos * 3, 255 - pos * 3, 0); }
     }
-    offset = (offset + 1) % 256;
-    ws281x.render(pixelData);
-  }, 1000 / 30);
 
-  console.log('Press <ctrl>+C to exit.');
-
-  // rainbow-colors, taken from http://goo.gl/Cs3H0v
-  function colorwheel(pos) {
-    pos = 255 - pos;
-    if (pos < 85) { return rgb2Int(255 - pos * 3, 0, pos * 3); }
-    else if (pos < 170) { pos -= 85; return rgb2Int(0, pos * 3, 255 - pos * 3); }
-    else { pos -= 170; return rgb2Int(pos * 3, 255 - pos * 3, 0); }
-  }
-
-  function rgb2Int(r, g, b) {
-   return ((r & 0xff) << 16) + ((g & 0xff) << 8) + (b & 0xff);
+    function rgb2Int(r, g, b) {
+    return ((r & 0xff) << 16) + ((g & 0xff) << 8) + (b & 0xff);
+    }
   }
 }
 
