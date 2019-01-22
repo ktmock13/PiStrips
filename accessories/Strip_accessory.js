3ef4729933bec0d1c78f8629f43859893631c6c1
ktmock13 <ktmock13@gmail.com>
2019-01-21 18:38:08 -0800

    update

--- accessories/Strip_accessory.js
+++ accessories/Strip_accessory.js
@@ -77,29 +77,19 @@ function _runRainbow() {
 function rgb2Int(r, g, b) {
   return ((r & 0xff) << 16) + ((g & 0xff) << 8) + (b & 0xff);
 }
-function hslToRgb(h, s, l){
-  var r, g, b;
-
-  if(s == 0){
-      r = g = b = l; // achromatic
-  }else{
-      var hue2rgb = function hue2rgb(p, q, t){
-          if(t < 0) t += 1;
-          if(t > 1) t -= 1;
-          if(t < 1/6) return p + (q - p) * 6 * t;
-          if(t < 1/2) return q;
-          if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
-          return p;
-      }
-
-      var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-      var p = 2 * l - q;
-      r = hue2rgb(p, q, h + 1/3);
-      g = hue2rgb(p, q, h);
-      b = hue2rgb(p, q, h - 1/3);
+function hslToRgb(hue, sat, light) {
+  var t1, t2, r, g, b;
+  hue = hue / 60;
+  if ( light <= 0.5 ) {
+    t2 = light * (sat + 1);
+  } else {
+    t2 = light + sat - (light * sat);
   }
-
-  return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+  t1 = light * 2 - t2;
+  r = hueToRgb(t1, t2, hue + 2) * 255;
+  g = hueToRgb(t1, t2, hue) * 255;
+  b = hueToRgb(t1, t2, hue - 2) * 255;
+  return [r,g,b];
 }
 
 var LightController = {
