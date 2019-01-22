0be059a38e77f5e0cdbf2750b8b137dc3b32a034
ktmock13 <ktmock13@gmail.com>
2019-01-21 17:30:10 -0800

    update

--- accessories/Strip_accessory.js
+++ accessories/Strip_accessory.js
@@ -9,6 +9,45 @@ let interval = 0;
 function _kill() {
   clearInterval(interval);
   ws281x.reset();
+  interval = 0;
+}
+
+function _runRainbow() {
+  var NUM_LEDS = 80,
+  pixelData = new Uint32Array(NUM_LEDS);
+
+  ws281x.init(NUM_LEDS);
+
+  // ---- trap the SIGINT and reset before exit
+  process.on('SIGINT', function () {
+    ws281x.reset();
+    process.nextTick(function () { process.exit(0); });
+  });
+
+
+  // ---- animation-loop
+  var offset = 0;
+  interval = setInterval(function () {
+    for (var i = 0; i < NUM_LEDS; i++) {
+      pixelData[i] = colorwheel((offset + i) % 256);
+    }
+    offset = (offset + 1) % 256;
+    ws281x.render(pixelData);
+  }, 1000 / 30);
+
+  console.log('Press <ctrl>+C to exit.');
+
+  // rainbow-colors, taken from http://goo.gl/Cs3H0v
+  function colorwheel(pos) {
+    pos = 255 - pos;
+    if (pos < 85) { return rgb2Int(255 - pos * 3, 0, pos * 3); }
+    else if (pos < 170) { pos -= 85; return rgb2Int(0, pos * 3, 255 - pos * 3); }
+    else { pos -= 170; return rgb2Int(pos * 3, 255 - pos * 3, 0); }
+  }
+
+  function rgb2Int(r, g, b) {
+   return ((r & 0xff) << 16) + ((g & 0xff) << 8) + (b & 0xff);
+  }
 }
 
 var LightController = {
@@ -23,56 +62,15 @@ var LightController = {
 
   outputLogs: false, //output logs
 
-  _runRainbow: function() {
-    var NUM_LEDS = 80,
-    pixelData = new Uint32Array(NUM_LEDS);
   
-    ws281x.init(NUM_LEDS);
-  
-    // ---- trap the SIGINT and reset before exit
-    process.on('SIGINT', function () {
-      ws281x.reset();
-      process.nextTick(function () { process.exit(0); });
-    });
-  
-  
-    // ---- animation-loop
-    var offset = 0;
-    interval = setInterval(function () {
-      for (var i = 0; i < NUM_LEDS; i++) {
-        pixelData[i] = colorwheel((offset + i) % 256);
-      }
-      offset = (offset + 1) % 256;
-      ws281x.render(pixelData);
-    }, 1000 / 30);
-  
-    console.log('Press <ctrl>+C to exit.');
-  
-    // rainbow-colors, taken from http://goo.gl/Cs3H0v
-    function colorwheel(pos) {
-      pos = 255 - pos;
-      if (pos < 85) { return rgb2Int(255 - pos * 3, 0, pos * 3); }
-      else if (pos < 170) { pos -= 85; return rgb2Int(0, pos * 3, 255 - pos * 3); }
-      else { pos -= 170; return rgb2Int(pos * 3, 255 - pos * 3, 0); }
-    }
-  
-    function rgb2Int(r, g, b) {
-     return ((r & 0xff) << 16) + ((g & 0xff) << 8) + (b & 0xff);
-    }
-  },
-
-  _kill: function() {
-    clearInterval(interval);
-    ws281x.reset();
-  },
 
   setPower: function(status) { //set power of accessory
     if(this.outputLogs) console.log("Turning the '%s' %s", this.name, status ? "on" : "off");
     this.power = status;
     if(status){
-      this._runRainbow();
+      _runRainbow();
     } else {
-      this._kill();
+      _kill();
     }
   },
 
@@ -84,7 +82,8 @@ var LightController = {
   setBrightness: function(brightness) { //set brightness
     if(this.outputLogs) console.log("Setting '%s' brightness to %s", this.name, brightness);
     this.brightness = brightness;
-    if (!brightness)  this.setPower(false);
+    if(!brightness) _kill()
+    if(brightness && !interval) _runRainbow();
   },
 
   getBrightness: function() { //get brightness
